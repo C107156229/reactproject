559eeca51d4e8f671660ea94b1aee35ede3240fc
c107156229 <c107156229@nkust.edu.tw>
2021-04-15 12:01:43 +0800

    hw3

---
+++ 0415/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Clock from "./the3/clock";
+
+ReactDOM.render(
+  <React.Fragment>
+    <Clock />,
+  </React.Fragment>,
+  document.getElementById("root")
+);
